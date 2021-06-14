45c759462a64400de7cc79ef25fd39c897c65025
Christopher Krasniak <christopher.krasniak@internationalbrainlab.org>
2021-06-14 12:11:57 -0400

    added fetching data required for receptive field mapping

--- wfield/fetchONE.py
+++ wfield/fetchONE.py
@@ -6,31 +6,67 @@ from oneibl.one import ONE
 from pathlib import Path
 import numpy as np
 import sys, getopt
+import pandas as pd
+## receptive field mapping in widefield imaging
+
+from oneibl.one import ONE
+from pathlib import Path
+import numpy as np
+import sys, getopt
+import brainbox.io.one as bbone
+import os
+import brainbox.task.passive as passive
+import numpy as np
+import pickle
+one = ONE()
+
+
 one = ONE()
 def fetchONE(argv):
     _,args = getopt.getopt(argv,'abc:d:')
     
     localdisk = args[0]
     print(localdisk)
-    subject = localdisk.split('\\')[-3]
+    subject = localdisk.split('\\')[-2]
     date = localdisk.split('\\')[-1]
     print(subject,date)
     eid = one.search(subject=subject, date=date)
+    if type(eid) == list:
+        eid = eid[0]
     dtypes = ['_spikeglx_sync.channels', '_spikeglx_sync.polarities',
             '_spikeglx_sync.times']
     sync_data = one.load(eid, dataset_types=dtypes)
     data_path = one.path_from_eid(eid)
-    sync_path = Path(data_path[0], 'raw_ephys_data')
+    if type(data_path) == list:
+        data_path = data_path[0]
+    try:
+        sync_path = Path(data_path, 'raw_ephys_data')
 
-    ch = np.load(Path(sync_path, '_spikeglx_sync.channels.npy'))
-    pol = np.load(Path(sync_path, '_spikeglx_sync.polarities.npy'))
-    times = np.load(Path(sync_path, '_spikeglx_sync.times.npy'))
-    bpod_ch = 16
-    use_ch = ch == bpod_ch
-    bpod_times = times[use_ch]
-    bpod_gaps = np.diff(bpod_times)
-    np.save(Path(localdisk, 'bpod_times.npy'), bpod_times)
+        ch = np.load(Path(sync_path, '_spikeglx_sync.channels.npy'))
+        pol = np.load(Path(sync_path, '_spikeglx_sync.polarities.npy'))
+        times = np.load(Path(sync_path, '_spikeglx_sync.times.npy'))
+        bpod_ch = 16
+        use_ch = ch == bpod_ch
+        bpod_times = times[use_ch]
+        bpod_gaps = np.diff(bpod_times)
+        np.save(Path(localdisk, 'bpod_times.npy'), bpod_times)
+        
+        rf_map = bbone.load_passive_rfmap(eid, one=one)
+        rf_stim_times, rf_stim_pos, rf_stim_frames = passive.get_on_off_times_and_positions(rf_map)
 
+        # rf_stim_times - time of frame change
+        # rf_stim_pos - position of each voxel on (15 x 15) screen
+        # rf_stim_frames - frames at which stimulus was turned 'on' (grey to white) or 'off' (grey to
+        #                   black) at each position on screen
+
+        np.save(Path(localdisk, 'rf_stim_times.npy'),rf_stim_times)
+        np.save(Path(localdisk, 'rf_stim_pos.npy'),rf_stim_pos)
+
+        f = open(Path(localdisk,"rf_stim_frames.pkl"),"wb")
+        pickle.dump(rf_stim_frames,f)
+        f.close()
+    except:
+        print('No ephys data, proceeding to download behavior data')
     d_sets = ['trials.choice',
             'trials.contrastLeft',
             'trials.contrastRight',
